942e0e9428c2cece212418552a25e62ab86cda51
zshabb <95062713+zshabb@users.noreply.github.com>
2025-06-04 18:59:38 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from twilio.rest import Client
 import os
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/api/calls', methods=['GET'])
 def get_calls():
