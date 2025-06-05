cc26352fe23036fb685d70bc0b2405ec3251527b
zshabb <95062713+zshabb@users.noreply.github.com>
2025-06-04 20:12:24 -0400

    Update app.py

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ def get_calls():
 
         return jsonify([
             {
-                "from": c.__dict__.get('from', ''),
+                "from": c.__dict__.get('from_', ''),
                 "to": c.to,
                 "start_time": str(c.start_time),
                 "duration": c.duration,
